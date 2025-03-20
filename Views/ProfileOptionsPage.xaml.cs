6f4f272d18500c545459b2044542cc67e78886b1
TopTrickster <bobovlahov@gmail.com>
2025-03-20 22:24:40 +0200

    profile options changes

--- Views/ProfileOptionsPage.xaml.cs
+++ Views/ProfileOptionsPage.xaml.cs
@@ -6,4 +6,26 @@ public partial class ProfileOptionsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void OnWorkerSelected(object sender, EventArgs e)
+    {
+        WorkerSection.IsVisible = true;
+        RecruiterSection.IsVisible = false;
+    }
+
+    private void OnRecruiterSelected(object sender, EventArgs e)
+    {
+        WorkerSection.IsVisible = false;
+        RecruiterSection.IsVisible = true;
+    }
+
+    private async void OnWorkerRegister(object sender, EventArgs e)
+    {
+        await DisplayAlert("Success", "Worker profile completed!", "OK");
+    }
+
+    private async void OnRecruiterRegister(object sender, EventArgs e)
+    {
+        await DisplayAlert("Success", "Recruiter profile completed!", "OK");
+    }
+
 }
