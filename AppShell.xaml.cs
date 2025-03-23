3a65a3c5d192f55be0cdee8cf9e1f9a5c16086a8
Alex Tsvetanov <alex.tsvetanov0+github@gmail.com>
2025-03-24 00:46:46 +0200

    move to tabbed layout

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,24 +1,13 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Windows.Input;
-using Microsoft.Maui.Controls;
+﻿using System.Windows.Input;
 
 namespace MauiProjectDraft
 {
     public partial class AppShell : Shell
     {
-        public bool IsLoading { get; set; }
-
-        public bool IsLoaderVisible { get; set; }
 
         public AppShell()
         {
-            IsLoading = true;
-            OnPropertyChanged(nameof(IsLoading));
-            IsLoaderVisible = true;
-            OnPropertyChanged(nameof(IsLoaderVisible));
             InitializeComponent();
-
 
             Routing.RegisterRoute("MainPage", typeof(Views.MainPage));
             Routing.RegisterRoute("login", typeof(Views.LoginPage));
@@ -27,46 +16,13 @@ namespace MauiProjectDraft
             NavigateToLoginCommand = new Command(async () => await Shell.Current.GoToAsync("login"));
 
             NavigateToSignUpCommand = new Command(async () => await Shell.Current.GoToAsync("signup"));
-                
-            NavigateToHomeCommand = new Command(async () => await Shell.Current.GoToAsync("MainPage"));
 
             Navigating += (s, e) => Shell.Current.FlyoutIsPresented = false;
 
             BindingContext = this;
-            IsLoading = false;
-            OnPropertyChanged(nameof(IsLoading));
-            IsLoaderVisible = false;
-            OnPropertyChanged(nameof(IsLoaderVisible));
         }
 
         public ICommand NavigateToLoginCommand { get; private set; }
         public ICommand NavigateToSignUpCommand { get; private set; }
-        public ICommand NavigateToHomeCommand { get; private set; }
-
-        private async void FlyoutList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            string[] flyoutRoutes = { "MainPage", "MainPage", "MainPage", "login", "signup" };
-            FlyoutItem selected = ((FlyoutItem)e.CurrentSelection.First());
-            int i = 0;
-            foreach (IReadOnlyList<Element> list in FlyoutItems)
-            {
-                foreach (object item in list)
-                {
-                    if (selected == item)
-                    {
-                        IsLoading = true;
-                        OnPropertyChanged(nameof(IsLoading));
-                        IsLoaderVisible = true;
-                        OnPropertyChanged(nameof(IsLoaderVisible));
-                        await Shell.Current.GoToAsync(flyoutRoutes[i]);
-                        IsLoading = false;
-                        OnPropertyChanged(nameof(IsLoading));
-                        IsLoaderVisible = false;
-                        OnPropertyChanged(nameof(IsLoaderVisible));
-                    }
-                    i++;
-                }
-            }
-        }
     }
 }
